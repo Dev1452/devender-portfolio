b5a1f589e77d3780b7d5c8fa4ed529513577fbf0
Devender jain <devenderj67@gmail.com>
2025-06-02 22:49:43 +0530

    add integration to email for contact form

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function sendMail() {
+    let params = {
+        first_name : document.getElementById('firstName').value,
+        last_name : document.getElementById('lastName').value,
+        email : document.getElementById('email').value,
+        message : document.getElementById('message').value,
+        phone_no : document.getElementById('phoneNo').value,
+    }
+
+    emailjs.send("service_6dkzrvf", "template_6oftgya", params).then(alert("Email Send Successfully!"))
+}
