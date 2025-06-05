d1cc59a5897b8829f2dfd67f49f89b363ddd0ca9
Devender jain <devenderj67@gmail.com>
2025-06-05 16:11:26 +0530

    Form error resolve

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-function sendMail() {
-    let params = {
-        first_name : document.getElementById('firstName').value,
-        last_name : document.getElementById('lastName').value,
-        email : document.getElementById('email').value,
-        message : document.getElementById('message').value,
-        phone_no : document.getElementById('phoneNo').value,
-    }
+console.log("hello i");
 
-    emailjs.send("service_od7cqrq", "template_6oftgya", params).then(alert("Email Send Successfully!"))
+function sendMail(e) {
+    e.preventDefault();
+    console.log("hello i");
+    let params = {
+        first_name: document.getElementById('firstName').value,
+        last_name: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+        phone_no: document.getElementById('phoneNo').value,
+    }
+  
+    emailjs.send("service_od7cqrq", "template_6oftgya", params).then(
+        alert("Email Send Successfully!"),
+    );
+
+
 }
